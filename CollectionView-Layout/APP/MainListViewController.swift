cdd2c07aee606630c6e810b1b51c03bfe8724f51
User-K02 <user-k02@w14-3.macminivault.com>
2022-10-17 16:42:17 -0700

    Added another section with compositional layout

--- CollectionView-Layout/APP/MainListViewController.swift
+++ CollectionView-Layout/APP/MainListViewController.swift
@@ -6,27 +6,33 @@
 //
 import UIKit
 import Combine
-
  class MainListViewController: UICollectionViewController {
      
      //Cantidad de secciones que tendra el collectionView
-     enum Section: CaseIterable {
-         case main
+     enum Section: String, CaseIterable {
+         case grid = "Grid"
+         case main = "Main"
      }
+     
+     enum Group: Hashable {
+         case grid(UIColor)
+         case main(SimpsonList)
+     }
+     
     
     //DataSource 7 DatasourceSnapshot typealias
-    typealias DataSource = UICollectionViewDiffableDataSource<Section, SimpsonList>
-    typealias DataSourceSnapshot = NSDiffableDataSourceSnapshot<Section, SimpsonList>
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Group>
+    typealias DataSourceSnapshot = NSDiffableDataSourceSnapshot<Section, Group>
      
      //Subscripcion con Combine
      private var subscription: AnyCancellable?
+     
      //Acceso a mi modelo y ViewModel
-     var listData = [SimpsonList]()
      private var viewModel: ListViewModel
     
     init(viewModel: ListViewModel) {
         self.viewModel = viewModel
-        super.init(collectionViewLayout: Self.configurationLayout())
+        super.init(collectionViewLayout: MainListViewController.configurationLayout())
     }
     
     required init?(coder: NSCoder) {
@@ -34,22 +40,28 @@ import Combine
     }
 
      //Registro de mi celda de la forma mas actualizada
-     let listcell = UICollectionView.CellRegistration<UICollectionViewListCell, SimpsonList> { cell, indexPath, model in
+     let gridcell = UICollectionView.CellRegistration<UICollectionViewCell, UIColor> { cell, _, color in
+         cell.backgroundColor = color
+     }
+     
+     let listcell = UICollectionView.CellRegistration<UICollectionViewListCell, SimpsonList> { cell, _, model in
          var listcontentConfiguration = UIListContentConfiguration.cell()
+         
          listcontentConfiguration.text = model.name.lowercased()
+         listcontentConfiguration.image = UIImage(systemName: "star")
          cell.contentConfiguration = listcontentConfiguration }
-    
-    lazy var dataSource: DataSource = {
-        let datasource = DataSource(collectionView: collectionView) { collectionView, indexPath, model -> UICollectionViewCell in
-            return collectionView.dequeueConfiguredReusableCell(using: self.listcell, for: indexPath, item: model)
-        }
-        return datasource
-    }()
+
     
      override func viewDidLoad() {
          super.viewDidLoad()
-         viewModel.loadData()
+         setUI()
          bindUI()
+         
+     }
+     
+     private func setUI() {
+         collectionView.backgroundColor = .white
+         viewModel.loadData()
      }
      
      //Capturando mis datos mediante el uso de combine.
@@ -66,20 +78,79 @@ import Combine
          }
      }
      
+     lazy var dataSource: DataSource = {
+         let datasource = DataSource(collectionView: collectionView) {[unowned self] collectionView, indexPath, item -> UICollectionViewCell in
+             switch item {
+             case .grid (let color):
+                 return collectionView.dequeueConfiguredReusableCell(using: self.gridcell, for: indexPath, item: color)
+                 
+             case .main(let model):
+                 return collectionView.dequeueConfiguredReusableCell(using: self.listcell, for: indexPath, item: model)
+             }
+         }
+         return datasource
+     }()
+     
+     
      //Configuracion de mi snapshot
      private func configurationSnapshot(simpsonList: [SimpsonList]) {
          var snapshot = DataSourceSnapshot()
-         snapshot.appendSections(Section.allCases)
-         snapshot.appendItems(simpsonList)
-         Section.allCases.forEach { snapshot.appendItems(listData, toSection: $0)
+         
+         let list =  (simpsonList.prefix (20))
+         let listMain = list.map { simson in
+             Group.main(simson)
          }
+         let listColor = [UIColor.purple, UIColor.red, UIColor.green, UIColor.brown,UIColor.systemPink,UIColor.systemCyan].map { color in
+             Group.grid(color)
+         }
+        
+         snapshot.appendSections(Section.allCases)
+         snapshot.appendItems(listMain,toSection: .main)
+         snapshot.appendItems(listColor,toSection: .grid)
+         
          dataSource.apply(snapshot)
      }
      
      static private func configurationLayout() -> UICollectionViewLayout {
-         let config = UICollectionLayoutListConfiguration(appearance: .grouped)
-         let layout = UICollectionViewCompositionalLayout.list(using: config)
          
+         let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection in
+             
+             
+             let sectionKind = Section.allCases [sectionIndex]
+             switch sectionKind {
+             case .grid:
+                 
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.045),
+                                                      heightDimension: .fractionalHeight(0.80))
+                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                 
+                 item.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                              leading: 5,
+                                                              bottom: 0,
+                                                              trailing: 0)
+
+                 let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.10))
+                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+                 
+                 let section = NSCollectionLayoutSection(group: group)
+                 section.orthogonalScrollingBehavior = .continuous
+                 return section
+                 
+             case .main:
+                 let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                       heightDimension: .fractionalHeight(1))
+                  let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                 
+                 let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.050))
+                 
+                 
+                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+                 
+                 
+                 let section = NSCollectionLayoutSection(group: group)
+                 return section
+             }
+         }
          return layout
      }
 }
